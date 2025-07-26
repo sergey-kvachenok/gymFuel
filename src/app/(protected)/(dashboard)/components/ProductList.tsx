3b90377f3312d1794ec39d1a0dc6dd70f96bfd28
Siarhei Kvachonak <siarheikvachonak@MacBook-Pro-Siarhei-2.local>
2025-07-26 14:56:23 +0200

    Fix layout

--- src/app/(protected)/(dashboard)/components/ProductList.tsx
+++ src/app/(protected)/(dashboard)/components/ProductList.tsx
@@ -1,9 +1,9 @@
 'use client';
 import { useState } from 'react';
-import { trpc } from '../../../lib/trpc-client';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Card } from '@/components/ui/card';
+import { trpc } from '@/lib/trpc-client';
 
 const nutritionFields = [
   {
